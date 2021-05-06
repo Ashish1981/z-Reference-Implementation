b26d0fc09e0fd70e372c3c2796a5d24420b992ac
Ashish K Sahoo <ashissah@in.ibm.com>
2021-05-06 10:15:19 +0530

    Fix plumber
    Signed-off-by: Ashish K Sahoo <ashissah@in.ibm.com>

--- plumb_1.sh
+++ plumb_1.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 export LD_LIBRARY_PATH=/usr/lib/jvm/java-11-openjdk-s390x/lib/server:/usr/lib/jvm/java-11-openjdk-s390x
 export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-s390x
-/usr/local/bin/Rscript /srv/shiny-server/run-myfile1.R
+/usr/local/bin/r /srv/shiny-server/run-myfile1.R
